d4e438450be3df8b0a754803306e4302b8e4fade
nacho131994 <nachommunoz13@gmail.com>
2023-11-24 19:59:08 +0100

    feature: registro nuevos equipos y traigo de la api sus datos

--- src/Sections/HomeLogged.jsx
+++ src/Sections/HomeLogged.jsx
@@ -6,6 +6,7 @@ const HomeLogged = () => {
   const [showTeamCreation, setShowTeamCreation] = useState(false);
   const [teamName, setTeamName] = useState("");
   const [stadiumName, setStadiumName] = useState("");
+  const [teamExists, setTeamExists] = useState(false);
 
   useEffect(() => {
     fetch("https://footb.onrender.com/v2/team/", {
@@ -21,6 +22,28 @@ const HomeLogged = () => {
       }
     });
   }, []);
+
+  //CREAR EQUIPO
+  async function handleCreateTeam(teamName, stadiumName) {
+    await fetch(
+      `https://footb.onrender.com/v2/team/?name=${teamName}&stadium=${stadiumName}`,
+      {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          Authorization: "Bearer " + JSON.parse(localStorage.getItem("token")),
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: "",
+      }
+    ).then((res) => {
+      if (res.ok) {
+        console.log(res);
+        setTeamExists(true);
+        setShowTeamCreation(false);
+      }
+    });
+  }
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -57,11 +80,29 @@ const HomeLogged = () => {
                 ></input>
 
                 <div className="create-team-buttons">
-                  {teamName && stadiumName && <button>ACEPTAR </button>}
+                  {teamName && stadiumName && (
+                    <button
+                      onClick={() => handleCreateTeam(teamName, stadiumName)}
+                    >
+                      ACEPTAR
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
           </>
+        )}
+        {teamExists && (
+          <div className="team-exists-container">
+            <div className="team-and-stadium">
+              <p>Nombre del equipo:</p>
+              <p>{teamName}</p>
+            </div>
+            <div className="team-and-stadium">
+              <p>Nombre del estadio:</p>
+              <p>{stadiumName}</p>
+            </div>
+          </div>
         )}
       </div>
     </>
