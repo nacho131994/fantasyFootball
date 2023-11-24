037561fca8f8ee259b1a56b248bd06bb0b0ae6a7
nacho131994 <nachommunoz13@gmail.com>
2023-11-24 20:24:55 +0100

    hotfix:el equipo permanece aun cerrando sesion

--- src/Sections/HomeLogged.jsx
+++ src/Sections/HomeLogged.jsx
@@ -1,5 +1,116 @@
+// import React, { useState, useEffect } from "react";
+// //STYLES
+// import "../Sections/HomeLogged.css";
+
+// const HomeLogged = () => {
+//   const [showTeamCreation, setShowTeamCreation] = useState(false);
+//   const [teamName, setTeamName] = useState("");
+//   const [stadiumName, setStadiumName] = useState("");
+//   const [teamExists, setTeamExists] = useState(false);
+
+//   useEffect(() => {
+//     fetch("https://footb.onrender.com/v2/team/", {
+//       headers: {
+//         accept: "application/json",
+//         Authorization: "Bearer " + JSON.parse(localStorage.getItem("token")),
+//       },
+//     }).then((res) => {
+//       if (res.ok) {
+//         setShowTeamCreation(false);
+//       } else {
+//         setShowTeamCreation(true);
+//       }
+//     });
+//   }, []);
+
+//   //CREAR EQUIPO
+//   async function handleCreateTeam(teamName, stadiumName) {
+//     await fetch(
+//       `https://footb.onrender.com/v2/team/?name=${teamName}&stadium=${stadiumName}`,
+//       {
+//         method: "POST",
+//         headers: {
+//           accept: "application/json",
+//           Authorization: "Bearer " + JSON.parse(localStorage.getItem("token")),
+//           "Content-Type": "application/x-www-form-urlencoded",
+//         },
+//         body: "",
+//       }
+//     ).then((res) => {
+//       if (res.ok) {
+//         console.log(res);
+//         setTeamExists(true);
+//         setShowTeamCreation(false);
+//       }
+//     });
+//   }
+
+//   const handleInputChange = (e) => {
+//     const { name, value } = e.target;
+//     if (name === "teamName") {
+//       setTeamName(value);
+//     } else if (name === "stadiumName") {
+//       setStadiumName(value);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className="create-team-container">
+//         {showTeamCreation && (
+//           <>
+//             <div className="create-team">
+//               <div className="create-team-title">
+//                 <h1>CREA TU EQUIPO</h1>
+//               </div>
+//               <div className="create-team-body">
+//                 <input
+//                   placeholder="Nombre del Equipo"
+//                   type="text"
+//                   name="teamName"
+//                   value={teamName}
+//                   onChange={handleInputChange}
+//                 ></input>
+//                 <input
+//                   placeholder="Nombre del Estadio"
+//                   type="text"
+//                   name="stadiumName"
+//                   value={stadiumName}
+//                   onChange={handleInputChange}
+//                 ></input>
+
+//                 <div className="create-team-buttons">
+//                   {teamName && stadiumName && (
+//                     <button
+//                       onClick={() => handleCreateTeam(teamName, stadiumName)}
+//                     >
+//                       ACEPTAR
+//                     </button>
+//                   )}
+//                 </div>
+//               </div>
+//             </div>
+//           </>
+//         )}
+//         {teamExists && (
+//           <div className="team-exists-container">
+//             <div className="team-and-stadium">
+//               <p>Nombre del equipo:</p>
+//               <p>{teamName}</p>
+//             </div>
+//             <div className="team-and-stadium">
+//               <p>Nombre del estadio:</p>
+//               <p>{stadiumName}</p>
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default HomeLogged;
 import React, { useState, useEffect } from "react";
-//STYLES
 import "../Sections/HomeLogged.css";
 
 const HomeLogged = () => {
@@ -7,25 +118,36 @@ const HomeLogged = () => {
   const [teamName, setTeamName] = useState("");
   const [stadiumName, setStadiumName] = useState("");
   const [teamExists, setTeamExists] = useState(false);
+  const [teamInfo, setTeamInfo] = useState(null);
 
-  useEffect(() => {
-    fetch("https://footb.onrender.com/v2/team/", {
+  // Función para verificar si el usuario tiene un equipo
+  const checkIfUserHasTeam = async () => {
+    const response = await fetch("https://footb.onrender.com/v2/team/", {
       headers: {
         accept: "application/json",
         Authorization: "Bearer " + JSON.parse(localStorage.getItem("token")),
       },
-    }).then((res) => {
-      if (res.ok) {
-        setShowTeamCreation(false);
-      } else {
-        setShowTeamCreation(true);
-      }
     });
+
+    if (response.ok) {
+      const teamData = await response.json();
+      setTeamInfo(teamData);
+      setTeamExists(true);
+      setShowTeamCreation(false);
+    } else {
+      setTeamExists(false);
+      setShowTeamCreation(true);
+    }
+  };
+
+  useEffect(() => {
+    // Verificar si el usuario tiene un equipo al cargar el componente
+    checkIfUserHasTeam();
   }, []);
 
-  //CREAR EQUIPO
+  // CREAR EQUIPO
   async function handleCreateTeam(teamName, stadiumName) {
-    await fetch(
+    const response = await fetch(
       `https://footb.onrender.com/v2/team/?name=${teamName}&stadium=${stadiumName}`,
       {
         method: "POST",
@@ -36,15 +158,15 @@ const HomeLogged = () => {
         },
         body: "",
       }
-    ).then((res) => {
-      if (res.ok) {
-        console.log(res);
-        setTeamExists(true);
-        setShowTeamCreation(false);
-      }
-    });
+    );
+
+    if (response.ok) {
+      // Después de crear el equipo, verifica nuevamente si el usuario tiene un equipo
+      checkIfUserHasTeam();
+    }
   }
 
+  // Manejo de cambios en los inputs
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     if (name === "teamName") {
@@ -54,6 +176,7 @@ const HomeLogged = () => {
     }
   };
 
+  // Resto del código JSX
   return (
     <>
       <div className="create-team-container">
@@ -96,11 +219,11 @@ const HomeLogged = () => {
           <div className="team-exists-container">
             <div className="team-and-stadium">
               <p>Nombre del equipo:</p>
-              <p>{teamName}</p>
+              <p>{teamInfo.name}</p>
             </div>
             <div className="team-and-stadium">
               <p>Nombre del estadio:</p>
-              <p>{stadiumName}</p>
+              <p>{teamInfo.stadium}</p>
             </div>
           </div>
         )}
